67360a55d7ac2df2c028d65167aef31b9815c610
AlfonsoConejo <alfonso.conejo@outlook.com>
2025-08-07 14:56:47 -0600

    Menú lateral para crear nueva sesión

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('El código está funcionando');
-
 let listaPomodoros = JSON.parse(localStorage.getItem('listaPomodoros')) || '';
 
 if(listaPomodoros === '' || listaPomodoros.length === 0){
@@ -9,5 +7,26 @@ if(listaPomodoros === '' || listaPomodoros.length === 0){
     
 }
 
+//Obtenemos del DOM los valores para manipular la ventana modal de "Nueva sesión"
+const botonNuevaSesion = document.getElementById('btnNuevaSesion');
+const botonCerrarPanel = document.getElementById('cerrarPanel');
+const panel = document.getElementById('formularioNuevoPomodoro');
 
-const botonNuevaSesion = document.getElementById('');
+
+
+//Ejecutamos una acción al hacer click sobre el botón de "Nueva sesión"
+botonNuevaSesion.addEventListener('click', ()=>{
+    panel.classList.add('abierto');
+});
+
+//Ejecutamos acción al cerrar la ventana modal de "Nueva sesión"
+botonCerrarPanel.addEventListener('click', ()=>{
+    panel.classList.remove('abierto');
+});
+
+//Cerramos la modal el presionar ESC
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+    panel.classList.remove('abierto');
+  }
+});
