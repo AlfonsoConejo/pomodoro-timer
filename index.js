21e9a9be8c589b4ffd75cd2280876a66be30dff3
AlfonsoConejo <alfonso.conejo@outlook.com>
2025-08-07 19:03:46 -0600

    new session form design complete

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const arrayfondos= [
+    {id:1 , location: "./assets/fondos/17018-4k.jpg"},
+    {id:2 , location: "./assets/fondos/202797.jpg"},
+    {id:3 , location: "./assets/fondos/202827.jpg"},
+    {id:4 , location: "./assets/fondos/202833.jpg"},
+    {id:5 , location: "./assets/fondos/202901.jpg"},
+    {id:6 , location: "./assets/fondos/estructura-abstracta.jpg"},
+    {id:7 , location: "./assets/fondos/fondo-cubos.jpg"},
+    {id:8 , location: "./assets/fondos/mountains-fog-landscape.jpg"},
+    {id:9 , location: "./assets/fondos/mountains-fog-trees.jpg"}
+];
+
 let listaPomodoros = JSON.parse(localStorage.getItem('listaPomodoros')) || '';
 
 if(listaPomodoros === '' || listaPomodoros.length === 0){
@@ -11,8 +23,48 @@ if(listaPomodoros === '' || listaPomodoros.length === 0){
 const botonNuevaSesion = document.getElementById('btnNuevaSesion');
 const botonCerrarPanel = document.getElementById('cerrarPanel');
 const panel = document.getElementById('formularioNuevoPomodoro');
+const contenedorFondos = document.getElementById('contenedorFondos');
+
+let fondosCargados = '';
+//recorremos el arreglo de fondos y los agregamos dentro de un div
+arrayfondos.forEach(imagen => {
+    const nuevoFondoCargado = `
+    
+    <div class="miniaturaFondo" style="background-image: url('${imagen.location}')"  data-imagen-id='${imagen.id}'></div>
+
+    </div>
+    `;
+
+    fondosCargados = fondosCargados + nuevoFondoCargado;
+
+    contenedorFondos.innerHTML= fondosCargados;
+});
+
+//Obtenemos todos los elementos que se llamen miniaturaFondo
+const miniaturasFondos = document.querySelectorAll('.miniaturaFondo'); 
 
 
+// Buscar la miniatura con id = 1 y marcarla como seleccionada
+const miniaturaPorDefecto = [...miniaturasFondos].find(
+  m => m.dataset.imagenId === '1'
+);
+
+//Marcamos como seleccionada la miniatura por defecto (1)
+if (miniaturaPorDefecto) {
+  miniaturaPorDefecto.classList.add('seleccionada');
+}
+
+miniaturasFondos.forEach(miniatura => {
+    miniatura.addEventListener('click', (e) => {
+        const id = e.currentTarget.dataset.imagenId;
+        //Deseleccionamos todas las imágenes
+        miniaturasFondos.forEach(imagen => {
+            imagen.classList.remove('seleccionada');
+        });
+        console.log(`Hiciste clic en la imagen con ID: ${id}`);
+        miniatura.classList.add('seleccionada');
+    });
+});
 
 //Ejecutamos una acción al hacer click sobre el botón de "Nueva sesión"
 botonNuevaSesion.addEventListener('click', ()=>{
